fa8c9bcd2f788614bb846d35c80b14e5e91b72b3
IqueMoraes <iquedemoraes@hotmail.com>
2021-09-22 14:40:40 -0300

    finalizada e estilizada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
 import Form from "./components/form";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 
 function App() {
   return (
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -23,12 +23,12 @@ export default function Form() {
       .string()
       .oneOf([yup.ref("password")], "Senha não correspondente")
       .required("Senha obrigatória"),
-    terms: yup.array().required("É preciso concordar com os termos."),
+    terms: yup.boolean().oneOf([true], "É preciso concordar com os termos."),
   });
 
   const {
     register,
-    handleSumbit,
+    handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(formSchema) });
 
@@ -40,50 +40,67 @@ export default function Form() {
   return (
     <div className="formBox">
       <h3>Formulário de inscrição</h3>
-      <form onSubmit={() => handleSumbit(imprimeNoConsole)}>
+      <form onSubmit={handleSubmit(imprimeNoConsole)}>
         <input
           placeholder="Escolha um nome para Usuário"
           {...register("user")}
         />
-        {errors.user?.message && <p>{errors.user.message}</p>}
+        {<p className="errorMsg">{errors.user && errors.user.message}</p>}
         <input
           placeholder="Digite seu nome completo"
           {...register("fullname")}
         />
-        {errors.fullname?.message && <p>{errors.fullname.message}</p>}
+        {
+          <p className="errorMsg">
+            {errors.fullname && errors.fullname.message}
+          </p>
+        }
 
         <input
           placeholder="Escolha um endereço de E-mail"
           {...register("email")}
         />
-        {errors.email?.message && <p>{errors.email.message}</p>}
+        {<p className="errorMsg">{errors.email && errors.email.message}</p>}
 
         <input
           placeholder="Confirme seu E-mail"
           {...register("emailconfirm")}
         />
-        {errors.emailconfirm?.message && <p>{errors.emailconfirm.message}</p>}
+        {
+          <p className="errorMsg">
+            {errors.emailconfirm && errors.emailconfirm.message}
+          </p>
+        }
         <div className="passwordBox">
-          <input
-            type="password"
-            placeholder="Crie sua senha difícil"
-            {...register("password")}
-          />
-          {errors.password?.message && <p>{errors.password.message}</p>}
-
-          <input
-            type="password"
-            placeholder="Confirme sua senha"
-            {...register("passwordconfirm")}
-          />
-          {errors.passwordconfirm?.message && (
-            <p>{errors.passwordconfirm.message}</p>
-          )}
+          <div className="alertPasswordBox">
+            <input
+              type="password"
+              placeholder="Crie sua senha difícil"
+              {...register("password")}
+            />
+            {
+              <p className="errorMsg">
+                {errors.password && errors.password.message}
+              </p>
+            }
+          </div>
+          <div className="alertPasswordBox">
+            <input
+              type="password"
+              placeholder="Confirme sua senha"
+              {...register("passwordconfirm")}
+            />
+            {
+              <p className="errorMsg">
+                {errors.passwordconfirm && errors.passwordconfirm.message}
+              </p>
+            }
+          </div>
         </div>
         <div className="termsBox">
           <input type="checkbox" {...register("terms")} />
           <p>Eu aceito os termos de uso da aplicação </p>
-          {errors.terms?.message && <p>{errors.terms.message}</p>}
+          {<p className="errorMsg">{errors.terms && errors.terms.message}</p>}
         </div>
 
         <button type="submit">REALIZAR CADASTRO</button>
